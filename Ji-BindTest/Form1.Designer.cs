2e4416362e673c87a86a5b4859609345dd066a5e
robertkajnak <hezirisz@gmail.com>
2018-02-12 14:41:16 +0100

    Added basic UX
    Basic Menus and the help text. No actual functionality

---
+++ Ji-BindTest/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Ji_BindTest
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteCtrlVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearWorkspaceCtrlDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpAboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelHelp = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(584, 435);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.DoubleClick += new System.EventHandler(this.tableLayoutPanel1_DoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1,
+            this.helpAboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.pasteCtrlVToolStripMenuItem,
+            this.clearWorkspaceCtrlDToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.openToolStripMenuItem.Text = "Open (Ctrl + O)";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pasteCtrlVToolStripMenuItem
+            // 
+            this.pasteCtrlVToolStripMenuItem.Name = "pasteCtrlVToolStripMenuItem";
+            this.pasteCtrlVToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.pasteCtrlVToolStripMenuItem.Text = "Paste (Ctrl+V)";
+            this.pasteCtrlVToolStripMenuItem.Click += new System.EventHandler(this.pasteCtrlVToolStripMenuItem_Click);
+            // 
+            // clearWorkspaceCtrlDToolStripMenuItem
+            // 
+            this.clearWorkspaceCtrlDToolStripMenuItem.Name = "clearWorkspaceCtrlDToolStripMenuItem";
+            this.clearWorkspaceCtrlDToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.clearWorkspaceCtrlDToolStripMenuItem.Text = "Clear Workspace (Ctrl+D)";
+            this.clearWorkspaceCtrlDToolStripMenuItem.Click += new System.EventHandler(this.clearWorkspaceCtrlDToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.closeToolStripMenuItem.Text = "Close (Escape)";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // helpAboutToolStripMenuItem
+            // 
+            this.helpAboutToolStripMenuItem.Name = "helpAboutToolStripMenuItem";
+            this.helpAboutToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.helpAboutToolStripMenuItem.Text = "Help/About";
+            this.helpAboutToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // labelHelp
+            // 
+            this.labelHelp.AutoSize = true;
+            this.labelHelp.Enabled = false;
+            this.labelHelp.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHelp.Location = new System.Drawing.Point(51, 104);
+            this.labelHelp.Name = "labelHelp";
+            this.labelHelp.Size = new System.Drawing.Size(478, 273);
+            this.labelHelp.TabIndex = 1;
+            this.labelHelp.Text = resources.GetString("labelHelp.Text");
+            // 
+            // FormMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.labelHelp);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Ji Blind Test";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteCtrlVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearWorkspaceCtrlDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label labelHelp;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem helpAboutToolStripMenuItem;
+    }
+}
+
---
+++ Ji-BindTest/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ji_BindTest
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+
+
+        #region Utility Classes and Functions
+
+        ///TODO - ob
+        private PictureBox addFileToList(string Filename)
+        {
+
+            return null;
+        }
+
+        #region File Insert and Load
+
+        //TODO - this
+        private void pasteItem()
+        {
+            IDataObject iData = Clipboard.GetDataObject();
+            //TODO check this
+            if (iData.GetDataPresent(DataFormats.FileDrop))
+            {
+                //addFileToList();
+            }
+
+            if (iData.GetDataPresent(DataFormats.FileDrop))
+            {
+                foreach (var v in Clipboard.GetFileDropList())
+                {
+                    try
+                    {
+                        //addPictureBox(ImageFromFile(v));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Text does not leat do a valid image file");
+                    }
+                }
+            }
+        }
+
+        //TODO
+        private void LoadFileFromDialog()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "All Files (*.*)|*.*|Image Files|*.jpg;*.png;*.bmp;*.gif;*.tiff;*.ico;";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filename in openFileDialog1.FileNames)
+                {
+                    addFileToList(filename);
+                }
+            }
+        }
+
+        //TODO
+        private void ClearWorkspace()
+        {
+            //selectedPictureBox = null;
+            //pictureBoxes.Clear();
+            System.GC.Collect();
+            labelHelp.Visible = true;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Event Handlers
+
+        #region Menu Events
+
+        #region File
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadFileFromDialog();
+        }
+
+        private void pasteCtrlVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pasteItem();
+        }
+
+        private void clearWorkspaceCtrlDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+            Application.Exit();
+        }
+
+        #endregion
+
+        #region Help
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String text = "Quick and easy way of creating a compilation/story from distinct images.\n" +
+                "Offers the possibility to rescale the image after generation, should it be necessary.\n" +
+                "\n\n" +
+                "The Ignore DPI feature (on by default) only consideres the resolution of the images on compositing.\n" +
+                "Disabling it will scale the images based on DPI, 1:1 pixel ratio being at 96DPI (value comes from Visual Studio or Microsoft or sthg).\n" +
+                "For example: putting a 182 DPI image with 800*600 pixels will result in an image with 400*300 pixels.\n" +
+                "\n\n" +
+                "\"Lock-Load Files\" locks the added files to the application so they can't be modified externally.\n" +
+                "Might fix some issues with some wierder file configurations, but is usually not necessary" +
+                "Only has effect on files added after changing the option (i.e. does not reload files already inside the application)\n" +
+                "\n\n" +
+                "To report any bugs of issues visit the repo: \n" +
+                "https://github.com/RobertKajnak/ImageStoryMerge";
+            String caption = "Help/About";
+            MessageBox.Show(text, caption);
+        }
+        #endregion
+
+        #endregion
+
+        #region Other Events
+
+        private void tableLayoutPanel1_DoubleClick(object sender, EventArgs e)
+        {
+            LoadFileFromDialog();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+}
